2d67402a3254bd5a1816041f595873b398ca27af
Ali Ganji <aliganji142879@gmail.com>
2024-02-13 13:56:13 +0330

    add tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
+class SignUpPageTest(TestCase):
+    username = 'username'
+    email = 'myusername@gmail.com'
+    age = 22
+
+    def test_signup_page_by_name_url(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_form(self):
+        user = get_user_model().objects.create_user(
+            username='username',
+            email='myusername@gmail.com',
+            age=22,
+        )
+        self.assertEqual(get_user_model().objects.count(), 1, )
+        self.assertEqual(get_user_model().objects.all()[0].username, user.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, user.email)
+        self.assertEqual(get_user_model().objects.all()[0].age, user.age)
--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local apps
+    'crispy_forms',
+    'crispy_bootstrap5',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
@@ -125,3 +127,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
